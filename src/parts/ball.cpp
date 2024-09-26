96b8d3fa3646ec4e1cc433d6096f13a62bfc76ca
michroch <105334350+michroch@users.noreply.github.com>
2024-09-26 13:41:29 -0400

    Update ball.cpp

--- src/parts/ball.cpp
+++ src/parts/ball.cpp
@@ -234,7 +234,7 @@ void BallEx::Render(const unsigned int renderMask)
          /* if (sy > sx) antiStretch.y = sx / sy; else antiStretch.x = sy / sx; */
          // balance stretching/enlarging to avoid having too much size difference between ball at the top / ball at the bottom
         antiStretch.x = 0.5f * (1.0f + sy / sx);
-        antiStretch.y = 0.6f * (1.0f + sx / sy);
+        antiStretch.y = 0.4f * (1.0f + sx / sy);
       }
    }
 
